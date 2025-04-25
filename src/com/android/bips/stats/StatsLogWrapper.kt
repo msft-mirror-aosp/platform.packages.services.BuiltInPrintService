b542881cbe06f514b44f5da35a6e1dcc85f46f68
Aaron Massey <aaronmassey@google.com>
2025-04-25 21:33:10 +0000

    stats: Add initial StatsLogger with request caps status event
    We want to record the result status when fetching capabilites from IPP
    printers.
    
    Add initial logger, associated async wrapper,
    bips_request_printer_capabilities_status event, and associated
    robolectric test.
    
    Bug: 390478410
    Test: validate logged events via adb and webui
    Test: atest BuiltInPrintServiceRobolectricTest
    Flag: com.android.bips.flags.printing_telemetry
    Change-Id: Ib72c2ceb1080fe99b5537dae99ed1bd4bc8c2eac

---
+++ src/com/android/bips/stats/StatsLogWrapper.kt
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.bips.stats
+
+// Thin wrapper around the generated atom logger for dependency
+// injection and testability
+
+// Kotlin does not offer package-private visibility modifier.
+// Clients outside of the package should use StatsAsyncLogger instead.
+
+// Not intended to be subclassed, left "open" for mocking.  internal
+// modifier isn't used as it doesn't play nice with @VisibleForTesting
+// annotation within package clients.
+open class StatsLogWrapper {
+    open fun internalRequestPrinterCapabilitiesStatus(getLocalCapsStatus: Int, secure: Boolean) {
+        BipsStatsLog.write(
+            BipsStatsLog.BIPS_REQUEST_PRINTER_CAPABILITIES_STATUS,
+            getLocalCapsStatus,
+            secure,
+        )
+    }
+}
